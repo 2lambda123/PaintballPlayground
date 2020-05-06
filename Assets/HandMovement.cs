130233834f2d11872acc4894e7e036143bb0c889
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-05-05 20:43:48 -0600

    Init. Hand orbs with very rough 'movement' script for hands only.

---
+++ Assets/HandMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    [SerializeField]
+    private bool isLeftHand;
+    public bool IsLeftHand
+    {
+        get { return isLeftHand; }
+        set { isLeftHand = value; }
+    }
+    private Quaternion baseRotation;
+    private OVRInput.Controller controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseRotation = transform.rotation;
+        controller = IsLeftHand ? OVRInput.Controller.LTouch : OVRInput.Controller.RTouch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        transform.position = OVRInput.GetLocalControllerPosition(controller);
+        transform.rotation = OVRInput.GetLocalControllerRotation(controller);
+    }
+}
