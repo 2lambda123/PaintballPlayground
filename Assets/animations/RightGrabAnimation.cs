20b34626276d692c56374b3ed7bf7026e59d2c44
code-witch <code-witch@users.noreply.github.com>
2020-05-19 18:04:56 -0600

    animation stuff

---
+++ Assets/animations/RightGrabAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightGrabAnimation : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) >= 0.01f) 
+        {
+            if (!anim.GetBool("isRightGrabbing"))
+            {
+                anim.SetBool("isRightGrabbing", true);
+            }
+        }
+        else
+        {
+            if (anim.GetBool("isRightGrabbing"))
+            {
+                anim.SetBool("isRightGrabbing", false);
+            }
+        }
+    }
+}
